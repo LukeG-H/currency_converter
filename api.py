1f1e744cdab2607413229e25d19317a37d5f659a
LukeG-H <lukegranthamhall@gmail.com>
2025-01-18 00:56:45 +0000

    LGH: initial commit: comments added

--- api.py
+++ api.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import requests
 
+# get some data from api:
 response = requests.get('https://randomuser.me/api')
 # print(response.status_code)
 # print(response.json())
 
+# format selected data (should be a function but this is fine for now):
 data = response.json()
 gender = data["results"][0]["gender"].upper()
 title = data["results"][0]["name"]["title"]
@@ -12,13 +14,9 @@ first_name = data["results"][0]["name"]["first"]
 last_name = data["results"][0]["name"]["last"]
 dob = data["results"][0]["dob"]["date"][:10]
 
-
-# print(f'Name: {title}. {first_name} {last_name}')
-# print(f'Gender: {gender}')
-# print(f'DOB: {dob}')
-
 random_person = f'{title}. {first_name} {last_name}'
 
+# display data using streamlit:
 st.write(f"""
 # Today's Random person is...
 *{random_person}*
